ba58faab6240a85bd1c7a2b46e9989a38379e8f3
reddy <mohanreddyderedla148@gmail.com>
2024-03-15 09:15:14 +0530

    frontend

---
+++ src/components/Admin/delete.js
@@ -0,0 +1,18 @@
+import AdminNavbar from "./adminnavbar";
+
+
+function DeleteComponent(){
+  
+   
+
+    return(
+        <div>
+            <AdminNavbar />
+            <h1>Deleted Successfully</h1>
+            
+    </div>
+
+       
+    )
+}
+export default DeleteComponent;
---
+++ src/components/Auth/login/logout.js
@@ -0,0 +1,8 @@
+function Logout(){
+    return(
+        <div>
+            <h3>Logout</h3>
+        </div>
+    )
+}
+export default Logout;
